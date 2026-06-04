9aeddfbd01e2682362f1dc6d290c8bf2af8ec2d9
deepan <deepanappu1999@gmail.com>
2026-06-04 13:06:19 +0530

    adding toast message in loigin component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const supabase = createClient(environment.supabaseUrl, environment.supabaseAnonKey);
 
@@ -10,6 +11,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideToastr(),
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withHashLocation()),
